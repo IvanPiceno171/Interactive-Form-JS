c86ef8c0b87bc9554071eefd752b765da5aeaaec
unknown <you@example.com>
2022-09-20 12:39:54 -0700

    ip-fs-2

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+
+
 const otherJobInput = document.getElementById('other-job-role');
 otherJobInput.classList.add('hiddenInput');
 
@@ -55,56 +57,65 @@ actBox.addEventListener('change', () => {
 const testAppend = document.getElementById('paypal')
 const payWith = document.getElementById('payment');
 const cardNumInput = document.getElementById('cc-num')
-payWith.addEventListener('change', () =>{
-   if(payWith.value == 'credit-card'){
-    
-   }
-   if(payWith.value == 'bitcoin'){
-    // console.log('works')
-    //    window.location.href = 'https://www.blockchain.com/btc/address/1BvBMSEYstWetqTFn5Au4m4GFg7xJaNVN2';
-   }
+const ccNotPicked = document.querySelectorAll('.payment-methods');
+payWith.addEventListener('change', ()=>{
+
+    if(payWith.value == 'paypal' || payWith.value == 'bitcoin'){
+        document.querySelector('#credit-card').style = 'display: none'
+    }
+    if(payWith.value == 'bitcoin'){
+
+    }
 })
 
 // check if name input is not empty
 const nameInput = document.getElementById('name');
+nameInput.focus();
 const emailInput = document.getElementById('email')
 const zipInput = document.getElementById('zip')
 const cvvInput = document.getElementById('cvv')
+const emailReg = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
+const nameReg = /^[a-zA-Z ]{2,30}$/
+const ccReg = /^(?:4[0-9]{12}(?:[0-9]{3})?)$/
+const zipReg = /(^\d{5}$)|(^\d{5}-\d{4}$)/
+const cvvReg = /^[0-9]{3,4}$/
 
-nameInput.addEventListener('click', () =>{
-    if(nameInput.value.length === 0){
+
+document.getElementById('regButton').addEventListener('click', (e) =>{
+    e.preventDefault()
+    
+    if(payWith.value == 'bitcoin'){
+        window.location.href = 'https://www.blockchain.com/btc/address/1BvBMSEYstWetqTFn5Au4m4GFg7xJaNVN2';
+    }
+     if (payWith.value == 'paypal') {
+         window.location.href = 'https://www.paypal.com/paypalme/1ZKCJQ6JQ';
+     }
+    if(!nameInput.value.match(nameReg)){
         document.getElementById('labelName').classList.add("not-valid");
         document.getElementById('name-hint').classList.remove('hint')
     }
     
-    if(!emailInput.value.includes('@') || !emailInput.value.includes('.com'))
-    {
+    if(!emailInput.value.match(emailReg)){
         document.getElementById('labelEmail').classList.add('not-valid')
         document.getElementById('email-hint').classList.remove('hint')
     }
-    if(actBox.querySelectorAll("input:checked").length === 0)
-    {
+    if(actBox.querySelectorAll("input:checked").length === 0){
         document.getElementById('activities').classList.add('not-valid')
         document.getElementById('activities-hint').classList.remove('hint')
     }
-    if (
-      (payWith.value == "credit-card" &&
-        cardNumInput.value.length < 15 && !cardNumInput.value.includes("0-9")) ||
-      cardNumInput.value.includes(" ") || cardNumInput.value.includes("-") &&
-      zipInput.value.length < 5 || !zipInput.value.includes('0-9')
-      && cvvInput.value.length < 3 || !cvvInput.value.includes('0-9')
-    ) {
-        console.log('works')
-        // document.getElementById('cc-hint').classList.remove('hint')
-        // document.getElementById('zip-hint').classList.remove('hint')
-        // document.getElementById('cvv-hint').classList.remove('hint')
+    if (!cardNumInput.value.match(ccReg)) {
+        document.getElementById("cc-hint").classList.add("not-valid");
+        document.getElementById('cc-hint').classList.remove('hint')
     }
-
-
+    if(!zipInput.value.match(zipReg)){
+        document.getElementById("zip-hint").classList.add("not-valid");
+        document.getElementById('zip-hint').classList.remove('hint')
+    }
+    if(!cvvInput.value.match(cvvReg)){
+        document.getElementById('cvv-hint').classList.add('not-valid')
+        document.getElementById('cvv-hint').classList.remove('hint')
+    }
 })
 
 
-document.getElementById('regButton').addEventListener('click', (e) =>{
-    // alert('win')
-    e.preventDefault()
-})
+
